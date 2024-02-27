146ca26462dc03890e18d4c66aa38fe00b3010c5
Franklin Dyer <franklin+github@dyer.me>
2024-02-26 20:28:43 -0700

    authentication does not belong in this app!

--- app.py
+++ app.py
@@ -4,75 +4,24 @@ from flask_minify import minify
 import random, string
 
 app = Flask(__name__)
-app.config.update(
-    MAIL_SUPPRESS_SEND = False
-)
-mail = Mail(app)
 
-app = Flask(__name__)
 minify(app=app, html=True, js=True, cssless=True)
-
-domain_name = "http://dev.franklin.dyer.me"
-
-approved_addresses = ["franklin@dyer.me", "george@dyer.me"]
-secure_access = "off";
-temporary_hashes = []
-
-def bwa_check_cookie(req):
-    if "bwa_authenticator" in req.cookies:
-        return True
-    else:
-        return False
 
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template("404.html"), 404
 
-@app.route('/login/<path:path>', methods=['POST', 'GET'])
-def test_login(path):
-    if request.method == 'POST':
-        user_email = request.form.get("user_email")
-        if user_email in approved_addresses:
-            new_hash = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
-            temporary_hashes.append(new_hash)
-            if len(temporary_hashes) > 10: del temporary_hashes[0]
-            msg = Message("Game access",sender="bw@dev.franklin.dyer.me",recipients=[user_email],body="Looks like you've requested access to my game! To see the beta version, go this url: "+domain_name+"/login/"+new_hash)
-            mail.send(msg)
-            return "Now check your email!"
-        else:
-            msg_fr = Message("Game access",sender="bw@dev.franklin.dyer.me",recipients=["franklin@dyer.me"], body="The following person has requested access to your game: "+user_email)
-            msg_user = Message("Game access", sender="bw@dev.franklin.dyer.me",recipients=[user_email], body="You've requested access to my game, but your email is not on the list of approved users. Permission has been requested.")
-            mail.send(msg_user)
-            mail.send(msg_fr)
-            return "Sorry, you don't currently have access to this game. An email has been sent requesting access."
-    elif request.method == 'GET':
-        if path in temporary_hashes:
-            resp = make_response(redirect(domain_name))
-            resp.set_cookie("bwa_authenticator","approved")
-            return resp
-        else:
-            return "Sorry, your access code is invalid."
-
 @app.route("/")
 def home():
-    if bwa_check_cookie(request) or secure_access == "off":
-        return render_template('home.html')
-    else:
-        return render_template("login.html")
+    return render_template('home.html')
 	
 @app.route("/page/<path:path>")
 def send_page(path):
-    if bwa_check_cookie(request) or secure_access == "off":
-        return render_template(path+".html")
-    else:
-        return "Sorry, you don't have permission to access this."
+    return render_template(path+".html")
 
 @app.route("/enemy/")
 def send_enemy():
-    if bwa_check_cookie(request) or secure_access == "off":
-	    return render_template("battle_screen.html")
-    else:
-        return "Sorry, you don't have permission to access this."
+	return render_template("battle_screen.html")
 
 @app.route('/js/<path:path>')
 def send_js(path):
